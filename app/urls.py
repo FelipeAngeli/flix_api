bd29905338acdfca7ec9cba98a542811482fcacc
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-11-13 16:26:12 -0300

    GenreRetriveUpdateDestroyView

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreateListView, genre_detail_view
+from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('genres/', GenreCreateListView.as_view(), name= 'genre-crate-list'),
-    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
+    path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(), name='genre-detail-view'),
 ]
