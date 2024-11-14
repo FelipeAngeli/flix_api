5d83a7a901318860a2983566b8825b64d7b64a40
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-11-14 10:59:13 -0300

    app actors

--- actors/models.py
+++ actors/models.py
@@ -52,7 +52,7 @@ class Actor(models.Model):
     name = models.CharField(max_length=200)
     birthday = models.DateField(null=True, blank=True)
     nationality = models.CharField(max_length=100, choices=NATIONALITY_CHOICES, null=True, blank=True)
-    biography = models.TextField(max_length= 300, null=True, blank=True)
+    biography = models.TextField(max_length= 600, null=True, blank=True)
 
     def __str__(self):
         return self.name
