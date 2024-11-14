0d49855fae1f51f88db16218705370a8e055e36d
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-11-14 16:14:52 -0300

    app movies

--- app/urls.py
+++ app/urls.py
@@ -3,14 +3,19 @@ from django.contrib import admin
 from django.urls import path
 from genres.views import GenreCreateListView, GenreRetriveUpdateDestroyView
 from actors.views import ActorCreateListView, ActorRetrieveUpdateDestroyView
+from movies.views import MovieCreateListView, MovieRetrieveUpdateDestroyView
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #genres
     path('genres/', GenreCreateListView.as_view(), name= 'genre-create-list'),
     path('genres/<int:pk>/', GenreRetriveUpdateDestroyView.as_view(), name='genre-detail-view'),
-
+    #actors
     path('actors/', ActorCreateListView.as_view(), name='actor-create-list'),
     path('actors/<int:pk>/', ActorRetrieveUpdateDestroyView.as_view(), name='actor-detail-view'),
+    #movies
+    path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDestroyView.as_view(), name='movie-detail-view'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from movies.models import Movie
+from movies.seriealizer import MovieSerializer
 
-# Create your views here.
+
+class MovieCreateListView(generics.ListCreateAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+
+    
+
+
