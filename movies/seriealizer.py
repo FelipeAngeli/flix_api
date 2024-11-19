b606491ff736247b0e92a1b9738c6b1502994829
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-11-19 10:51:40 -0300

    authentication

--- movies/seriealizer.py
+++ movies/seriealizer.py
@@ -1,8 +1,33 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
+class MovieModelSerializer(serializers.ModelSerializer):
+    #aqui add mais um campo no serializer para calcular a média de avaliações
+    rate = serializers.SerializerMethodField(read_only=True)
 
-class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    #aqui implemento o método para calcular a média de avaliações
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+
+        if rate:
+            return round(rate, 1)
+
+        return None
+     
+
+
+
+    def validate_release_date(self, value):
+        if value.year < 1900:
+            raise serializers.ValidationError('A data de lançamento não pode ser menor que 1900')
+        return value
+    
+    def validate_resume(self, value):
+        if not (10 <= len(value) <= 200):
+            raise serializers.ValidationError('O resumo deve ter entre 10 e 200 caracteres.')
+        return value
--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,15 @@
 from rest_framework import generics
 from movies.models import Movie
-from movies.seriealizer import MovieSerializer
+from movies.seriealizer import MovieModelSerializer
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieModelSerializer
 
     
 
