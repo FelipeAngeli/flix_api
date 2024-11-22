81b6a96a32f489fce0f28b8f65ebfff2753f3cb8
Felipe Angeli <59125006+FelipeAngeli@users.noreply.github.com>
2024-11-22 16:07:29 -0300

    pep8

--- app/urls.py
+++ app/urls.py
@@ -5,15 +5,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #authentication
     path('api/v1/', include('authentication.urls')),
-    #genres
     path('api/v1/', include('genres.urls')),
-    #actors
     path('api/v1/', include('actors.urls')),
-    #movies
     path('api/v1/', include('movies.urls')),
-    #reviews
     path('api/v1/', include('reviews.urls')),
-
 ]
